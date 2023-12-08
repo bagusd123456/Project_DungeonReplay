d1241fbe81688a2cbf8de686f3a94f668f3f4571
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2023-12-09 02:03:47 +0700

    [Bagus] Update Behavior di RoomRandomizer

--- Assets/Scripts/ProceduralGenerator/RoomRandomizer.cs
+++ Assets/Scripts/ProceduralGenerator/RoomRandomizer.cs
@@ -1,20 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RoomRandomizer : MonoBehaviour
 {
     public GameObject tempRoom;
-    public List<GameObject> roomPrefabList = new List<GameObject>();
+    public List<RoomProbability> roomPrefabList = new List<RoomProbability>();
+
+    public void Start()
+    {
+        RandomSpawn();
+    }
 
     //Random Spawn One from the list
     [ContextMenu("Random Spawn")]
     public void RandomSpawn()
     {
         tempRoom.SetActive(false);
-        int randomIndex = Random.Range(0, roomPrefabList.Count);
-        var GO = Instantiate(roomPrefabList[randomIndex], transform);
+        //int randomIndex = Random.Range(0, roomPrefabList.Count);
+
+        GameObject targetPrefab = ProbableElement.RandomWeightedElement(roomPrefabList).roomPrefab;
+
+        var GO = Instantiate(targetPrefab, transform);
         GO.transform.position = transform.localPosition;
         tempRoom = GO;
+    }
+}
+
+[Serializable]
+public class RoomProbability : ProbableElement
+{
+    public GameObject roomPrefab;
+    public float probability;
+
+    public float Probability
+    {
+        get => probability;
+        set
+        {
+
+        }
     }
 }
