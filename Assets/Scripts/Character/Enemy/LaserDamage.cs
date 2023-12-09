97dc3e6d066aee3927711e95f3eb56ded7dd7331
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2023-12-09 21:04:49 +0700

    [Bagus] Implement Gate Script

--- Assets/Scripts/Character/Enemy/LaserDamage.cs
+++ Assets/Scripts/Character/Enemy/LaserDamage.cs
@@ -1,20 +1,39 @@
+using System;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LaserDamage : MonoBehaviour
 {
     public int damageAmount = 5;
+
+    public float currentDamageTime = 0f;
+    public float damageInterval = 1.5f;
     private void OnTriggerStay(Collider other)
     {
         DamageContinuous(other);
     }
 
+    private void Update()
+    {
+        if (currentDamageTime > 0)
+        {
+            currentDamageTime -= Time.deltaTime;
+        }
+
+        else
+        {
+            currentDamageTime = 0;
+        }
+    }
+
     public void DamageContinuous(Collider other)
     {
-        if (other.GetComponent<PlayerHealth>() != null)
+        if (other.GetComponent<PlayerHealth>() != null && currentDamageTime <= 0)
         {
+            currentDamageTime = damageInterval;
             //Debug.Log("Player Take Damage");
             other.GetComponent<PlayerHealth>().TakeDamage(damageAmount);
         }
---
+++ Assets/Scripts/Controller/LaserWallController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LaserWallController : MonoBehaviour
+{
+    public List<Collider> gateList;
+
+    public float colliderSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetEntranceList();
+    }
+
+    private void OnDisable()
+    {
+        foreach (var gate in gateList)
+        {
+            gate.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var gate in gateList)
+        {
+            gate.gameObject.SetActive(gameObject.activeSelf);
+        }
+    }
+
+    [ContextMenu("Get Entrance List")]
+    public void GetEntranceList()
+    {
+        List<Collider> tempGateList = Physics.OverlapBox(transform.position, Vector3.one * colliderSize, Quaternion.identity).ToList();
+        for (int i = 0; i < tempGateList.Count; i++)
+        {
+            if (tempGateList[i].CompareTag("WallGate"))
+                gateList.Add(tempGateList[i]);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gateList.Count > 0)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+
+        Gizmos.DrawWireCube(transform.position, Vector3.one * colliderSize);
+    }
+}
